933e1d7400554ecb5697a0a36867d773e358604f
Enzo Marchetti <enzo.marchetti97@gmail.com>
2022-07-04 20:56:55 +0200

    player attack

---
+++ Assets/Scripts/S_Player/Attack.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using S_Combat;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    [SerializeField] Targeter targeter;
+
+    void Update()
+    {
+
+    }
+    public void SetTargetable()
+    {
+
+    }
+}
