ea08203f94d8f0569c6274e053e0da82c19c4a6b
Enzo Marchetti <enzo.marchetti97@gmail.com>
2022-07-04 18:43:51 +0200

    aDDED MOVEMENT

--- Assets/Scripts/S_Manager/InputManager.cs
+++ Assets/Scripts/S_Manager/InputManager.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class InputManager : MonoBehaviour
+public class InputManager : BaseSingleton<InputManager>
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public bool ClickedRightMouseButton()
     {
-        
+        if (!Mouse.current.rightButton.wasPressedThisFrame) return false;
+        {
+            return true;
+        }
+       
     }
 
-    // Update is called once per frame
-    void Update()
+    public Vector2 GetMousePos()
     {
-        
+        return Mouse.current.position.ReadValue();
     }
 }
--- Assets/Scripts/S_Player/PlayerMovement.cs
+++ Assets/Scripts/S_Player/PlayerMovement.cs
@@ -1,8 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using Mirror;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : NetworkBehaviour
 {
+    [SerializeField] LayerMask groundLayer;
+    NavMeshAgent agent;
+    Camera mainCamera;
 
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        mainCamera = Camera.main;
+    }
+    [Client]
+    void Update()
+    {
+        if (InputManager.instance.ClickedRightMouseButton())
+        {
+            SetMovePoint();
+        }
+       
+    }
+
+    [Command]
+    void MovePlayerTowardsPoint(Vector3 position)
+    {
+        bool validPosition = NavMesh.SamplePosition(position, out NavMeshHit hit, Mathf.Infinity, NavMesh.AllAreas);
+        Debug.Log(position);
+        if (!validPosition) return;
+        {
+            agent.SetDestination(hit.position);
+        }
+    }
+    [Client]
+    void SetMovePoint()
+    {
+        if (!hasAuthority) return;
+        {
+            Ray ray = mainCamera.ScreenPointToRay(InputManager.instance.GetMousePos());
+            bool validPos = Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, groundLayer);
+
+            if (!validPos) return;
+            {
+               MovePlayerTowardsPoint(hit.point);
+            }
+        }
+    }
 }
