b58355b125d751f9d177b062166d41463d2ce54f
George W <gecovin@protonmail.com>
2022-07-10 22:28:48 +0200

    BUUUUUUUUUUARGH

---
+++ Assets/Scripts/S_Manager/S_Controls/CameraController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+using Screen = UnityEngine.Device.Screen;
+
+namespace S_Manager.S_Controls
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] private GameObject camera;
+        [SerializeField] private float cameraSpeed;
+        [SerializeField] private float maxMagnitude;
+
+        private int _screenWidth, _screenHeight;
+        private Vector2 _mousePositionLastFrame;
+
+        private void Start()
+        {
+            _screenWidth = Screen.width;
+            _screenHeight = Screen.height;
+
+            _mousePositionLastFrame = Vector2.zero;
+            
+            if (camera != null) return;
+            if (Camera.main != null) camera = Camera.main.gameObject;
+        }
+
+        private void Update()
+        {
+            EdgePanning();
+            MiddleMouseMovement();
+        }
+
+        private Vector3 GetMouseDirection()
+        {
+            var direction = InputManager.instance.GetMousePos() - _mousePositionLastFrame;
+            _mousePositionLastFrame = InputManager.instance.GetMousePos();
+            
+            return new Vector3(direction.x, 0f, direction.y);
+        }
+
+        private void EdgePanning()
+        {
+            
+        }
+
+        
+        private void MiddleMouseMovement()
+        {
+            if (InputManager.instance.HoldingMiddleMouseButton)
+            {
+                var difference = Vector3.ClampMagnitude(GetMouseDirection() * cameraSpeed, maxMagnitude);
+                camera.transform.position += 
+                    difference * (Mathf.Pow(cameraSpeed, Mathf.Clamp(difference.magnitude, 0, maxMagnitude))
+                                  * Time.deltaTime);
+            }
+        }
+    }
+}
--- Assets/Scripts/S_Manager/S_Controls/InputManager.cs
+++ Assets/Scripts/S_Manager/S_Controls/InputManager.cs
@@ -8,5 +8,6 @@ public class InputManager : BaseSingleton<InputManager>
     public bool PressedAbilityButton() => Keyboard.current.digit1Key.wasPressedThisFrame;
     public bool ClickedRightMouseButton() => Mouse.current.rightButton.wasPressedThisFrame;
     public bool ClickedLeftMouseButton => Mouse.current.leftButton.wasPressedThisFrame;
+    public bool HoldingMiddleMouseButton => Mouse.current.middleButton.isPressed;
     public Vector2 GetMousePos() => Mouse.current.position.ReadValue();
 }
